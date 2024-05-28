e5ba9f37fca56b1c491fc249c03fde5ecb59815f
Amit Rathi <amit.rathi.k@gmail.com>
2024-05-28 14:00:52 -0400

    working on refactoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,15 +11,22 @@ import "./styles/main.scss";
 // import Grid from './views/grid';
 // import mobileNav from "./templates/partials/body/mobileNav";
 
+// require('angular');
+// require('ngstorage');
+// require('angular-bootstrap-select');
+// require('angular-sanitize');
+import initCanvas from "./utils/initCanvas";
 
-
-
-
-
+const visualBuilder = angular.module("visualBuilder", ["ngStorage", "ngSanitize", "angular-bootstrap-select", "ngDropzone"]);
+visualBuilder.controller("ConfigCtrl", ["$scope", "$http", "$localStorage", function(e, t, o) {}])
 (function() {
+  initCanvas();
+  jQuery(".preloaderimg").fadeOut(),
+  jQuery(".preloader").delay(200).fadeOut("slow").delay(200, function() {
+      jQuery(this).remove()
+  })
 
-
-  })();
+})();
   
 
 
